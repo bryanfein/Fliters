1afbccefa6d9b728eb029feeff12f19b0c68afc7
bryanfein <bryanfein@gmail.com>
2017-04-16 00:45:56 -0400

    Initial Commit

---
+++ Fliters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fliters
+//
+//  Created by Bryan Fein on 4/16/17.
+//  Copyright © 2017 Bryan Fein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fliters/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fliters
+//
+//  Created by Bryan Fein on 4/16/17.
+//  Copyright © 2017 Bryan Fein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
